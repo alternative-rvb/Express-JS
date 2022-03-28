3a8e4739fc846273837e81f30d1ca42840c6dd3c
Nicolas - home <alternative.rvb@gmail.com>
2022-03-28 15:03:04 +0200

    version-4

--- app/index-get.js
+++ app/index-get.js
@@ -5,22 +5,23 @@ const fs = require("fs");
 const {join} = require("path");
 
 const ENCODING = {encoding: "utf8"};
-const PATH = "./layouts"
-const TEMPLATE_PATH = join(PATH,"/baseof.html");
-const HOME_TITLE_PATH = join(PATH,"/home.title.html");
-const HOME_CONTENT_PATH = join(PATH,"/home.content.html");
+const PATH = "./layouts";
+
+const lireFichierHtml = (file) =>
+  fs.promises.readFile(join(PATH, file), ENCODING);
 
 module.exports = async () => {
   // Assemblage des fichiers
   // REVIEW
-  const BASE_OF_HTML = await fs.promises.readFile(TEMPLATE_PATH, ENCODING);
-  const TITLE_HTML = await fs.promises.readFile(HOME_TITLE_PATH, ENCODING);
-  const CONTENT_HTML = await fs.promises.readFile(HOME_CONTENT_PATH, ENCODING);
+  const BASE_OF_HTML = await lireFichierHtml("/baseof.html");
+  const TITLE_HTML = await lireFichierHtml("/home.title.html");
+  const CONTENT_HTML = await lireFichierHtml("/home.content.html");
 
   // Retourner les données
-  const HOME = BASE_OF_HTML
-  .replaceAll('{{TITLE}}', TITLE_HTML)
-  .replace('{{CONTENT}}', CONTENT_HTML)
+  const HOME = BASE_OF_HTML.replaceAll("{{TITLE}}", TITLE_HTML).replace(
+    "{{CONTENT}}",
+    CONTENT_HTML
+  );
 
   return HOME;
 };
